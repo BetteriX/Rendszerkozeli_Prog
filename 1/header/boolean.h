a9da1ba6a5b19c6e1c2a5c48948ce2488d9813da
BetteriX <szabozsolt430@gmail.com>
2025-02-17 13:28:23 +0100

    1_ora

---
+++ 1/header/boolean.h
@@ -0,0 +1,9 @@
+/*** A header file for symbolic boolean type ***/
+
+#define boolean_h
+
+typedef enum bool
+{
+    false,
+    true
+} boolean;
---
+++ 1/header/operator.h
@@ -0,0 +1,9 @@
+/*** Boolean operators ***/
+
+#ifndef boolean_h
+#include "boolean.h"
+#endif
+
+#define not !
+#define and &&
+#define or ||
